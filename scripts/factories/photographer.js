a9401382bf8a50cb479921cb47fdb0d460d62dfb
gaben.theo <gaben.theo@gmail.com>
2022-12-04 03:19:05 +0100

    [add] img portrait photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,6 +6,7 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
 
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
 
