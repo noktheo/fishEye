1c90bd91150696862b8af5dc43b66070ab213038
theo gaben <gaben.theo@gmail.com>
2023-01-26 19:36:04 +0100

    [edit] opti code -> page write photograph

--- scripts/pages/write_photographer.js
+++ scripts/pages/write_photographer.js
@@ -1,8 +1,3 @@
-/*let location = window.location.href;
-console.log(window.location);
-let id = new URLSearchParams('id');
-*/
-
 // get id
 const idPhotographer = localStorage.getItem('id');
 
@@ -20,101 +15,85 @@ async function getData() {
     return data;
 }
 
- //get data photographer
-async function getdataPhtotographer(data) {
-    dataPhotographer = [];
-
+//create photographer
+async function Photographer(data, target) {
     for (let i = 0; i < data.photographers.length; i++) {
         idP = data.photographers[i].id;
 
         if (idP == idPhotographer) {
-            dataPhotographer.push(data.photographers[i].name);
-            dataPhotographer.push(data.photographers[i].portrait);
-            dataPhotographer.push(data.photographers[i].price);
-            dataPhotographer.push(data.photographers[i].city);
-            dataPhotographer.push(data.photographers[i].tagline);
+            const avatar = `assets/photographers/${data.photographers[i].portrait}`;
+            newCreateElement('img', target, { src: avatar });
         }
         else {
             console.log('nn id');
         }
     }
-
-    return dataPhotographer
 }
 
 //get picture of photographer id => json array media
-async function getImgPhotographer(data) {
-    dataPhotographerImg = [];
+async function dataAnexPhotographer(data, parent) {
+    // let dataPhotographerImg = [];
 
     for (let x = 0; x < data.media.length; x++) {
         idP = data.media[x].photographerId;
 
         if (idP == idPhotographer) {
             console.log('yes media');
-            dataPhotographerImg.push(data.media[x].id);
-            dataPhotographerImg.push(data.media[x].photographerId);
-            dataPhotographerImg.push(data.media[x].title);
-            dataPhotographerImg.push(data.media[x].image);
-            dataPhotographerImg.push(data.media[x].likes);
-            dataPhotographerImg.push(data.media[x].date);
-            dataPhotographerImg.push(data.media[x].price);
+
+            //img
+            const get = data.media[x]
+
+            //no img get -> get video
+            let img;
+            if(get.image){
+                img = get.image;
+            }
+            else{
+                img = get.video;
+            }
+
+            const url = `assets/photographers/${img}`;
+            newCreateElement('img', parent[0], { src: url });
+            
+            //price
+            newCreateElement('h3', parent[1], { textContent: get.price });
+
+            //title
+            newCreateElement('h1', parent[0], { textContent: get.title, id: "machin" });
         }
         else {
             console.log('nn media');
         }
-
     }
-    return dataPhotographerImg
 }
 
+// create element
+const newCreateElement = (element, parent, json) => {
+    const balise = document.createElement(element);
+    // if (src) balise.setAttribute("src", src);
+    // if (alt) balise.setAttribute("alt", alt);
+    // if (textContent) balise.setAttribute("textContent", textContent);
+    Object.entries(json).forEach(([key,value]) => {
+        balise.setAttribute(key,value);
+    });
+    parent.appendChild(balise)
+}
 
 //create dom element
 async function pageDomPhotographer() {
     const fetchData = await getData();
-    const data = getdataPhtotographer(fetchData);
-    console.log('data -----------');
-    console.log(data);
 
-    const dataImg = getImgPhotographer(fetchData);
-    console.log('img -----------');
-    console.log(dataImg);
-
-    const portrait = data[1];
-    const imgNumber = dataImg[4];
-
-    //target
+    //target avatar
     const target = document.querySelector('.photograph-header');
 
-    const boxName = document.createElement('h3');
-    boxName.textContent = data[0];
+    await Photographer(fetchData, target);
 
-    //box title
-    const boxDescription = document.createElement('div');
-    const boxLocation = document.createElement('h4');
-    boxLocation.textContent = data[4];
-    const boxTagline = document.createElement('p');
-    boxTagline.textContent = data[5];
+    // target img
+    const targetimg = document.querySelector('.photograph-header');
 
-    //box avatar
-    const avatar = `assets/photographers/${portrait}`;
-    const avatarImg = document.createElement('img');
-    avatarImg.setAttribute("src", avatar);
+    //target price
+    const targetPrice = document.querySelector('.photograph-header');
 
-    //box img
-    const boxSectionImg = document.createElement('section');
-
-    const linkImg = `assets/photographers/${imgNumber}`;
-    const img = document.createElement('img');
-    img.setAttribute("src", linkImg);
-
-
-
-
-    /*write all balise on article -> .photograph-header*/
-    target.appendChild(boxName);
-    target.appendChild(avatarImg);
-    target.appendChild(boxDescription);
-    boxDescription.appendChild(boxLocation);
-    boxDescription.appendChild(boxTagline);
+    await dataAnexPhotographer(fetchData, [targetimg, targetPrice]);
 }
 pageDomPhotographer();
